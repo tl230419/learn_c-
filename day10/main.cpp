b1c9cc00cb5dfde93deac8eeac971dcebf2087e4
Allen <tl230419@163.com>
2020-06-02 21:06:09 +0800

    fix cmakelists file

--- day10/main.cpp
+++ day10/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include "config.h"
 #include "test.h"
+#include "test2.h"
 
 int main()
 {
     std::cout << "age = " << age << std::endl;
     int result = test::add(3, 4);
     std::cout << "result = " << result << std::endl;
+
+    int result2 = test2::add(8, 4);
+    std::cout << "result2 = " << result2 << std::endl;
 
     return 0;
 }
--- day10/test/test.h
+++ day10/test/test.h
@@ -1,8 +1,6 @@
 #ifndef TEST_H
 #define TEST_H
 
-//#include "test_exports.h"
-
 namespace test
 {
 
